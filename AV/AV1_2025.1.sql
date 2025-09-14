7e3cd910af567f2d31669708640f78c24907cfee
Felipe Tagawa <felipereisbkp@gmail.com>
2025-09-14 19:05:49 -0300

    feat: adiciona AV1

---
+++ AV/AV1_2025.1.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS AV1;
+CREATE DATABASE AV1;
+USE AV1;
+
+-- Responda a letra a) aqui
+DROP TABLE IF EXISTS Personagem;
+
+
+-- Respoda a letra b) aqui
+
+
+-- Responda a letra c) aqui
+
+
+-- Responda a letra d) aqui
+
+
+-- Responda a letra e) aqui
+
+
+-- Responda a letra f) aqui
+
+
+-- Responda a letra g) aqui
+
+
+-- Responda a letra h) aqui
+
+
+-- Responda a letra i) aqui
+
+
+-- Responda a letra j) aqui
+
+
+-- Responda a letra k) aqui
+
+
+-- Responda a letra l) aqui
+
