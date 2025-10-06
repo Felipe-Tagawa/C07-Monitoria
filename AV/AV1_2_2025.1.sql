cca87813fa3ff9358199e9e67c63770ea4079b6d
Felipe Tagawa <felipereisbkp@gmail.com>
2025-10-05 21:07:13 -0300

    feat: add AV1_2

---
+++ AV/AV1_2_2025.1.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS AV1;
+CREATE DATABASE AV1;
+USE AV1;
+
+DROP TABLE IF EXISTS Livros;
+-- Crie sua tabela aqui:
+
+-- faça a letra a aqui(inserções):
+
+-- faça a letra b aqui
+
+-- faça a letra c aqui
+
+-- faça a letra d aqui
+
+-- faça a letra e aqui
+
+-- faça a letra f aqui
+
+-- faça a letra g aqui
+
+-- faça a letra h aqui
+
+-- faça a letra i aqui
+
+-- faça a letra j aqui
+
+-- faça a letra k aqui
+ 
+-- faça a letra l aqui
